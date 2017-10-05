a4e1825eb6b06287c0354fcc7e26df74121037a7
Air <mkomor@czdz.pl>
2017-10-05 15:15:21 +0200

    Add new class files and add back button in skillVC

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -1,26 +1,23 @@
 //
-//  ViewController.swift
+//  LeagueVC.swift
 //  app-swoosh
 //
-//  Created by Air on 01.10.2017.
+//  Created by Air on 05.10.2017.
 //  Copyright © 2017 Air. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class LeagueVC: UIViewController {
 
-    @IBOutlet weak var backgroundImage: UIImageView!
-    @IBOutlet weak var swooshLogo: UIImageView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        // Do any additional setup after loading the view.
     }
-    
+
     @IBAction func goBack(back: UIStoryboardSegue) {
         
     }
 
 }
-
---
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Air on 01.10.2017.
+//  Copyright © 2017 Air. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var swooshLogo: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func goBack(back: UIStoryboardSegue) {
+        
+    }
+
+}
+

2b41bad8eeb063861aef2f00ef8b82d1a49b86ec
Air <mkomor@czdz.pl>
2017-10-05 17:39:08 +0200

    add programatical nextBtn functionality

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -15,6 +15,11 @@ class LeagueVC: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
 
     @IBAction func goBack(back: UIStoryboardSegue) {
         
--- app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class WelcomeVC: UIViewController {
 
-    @IBOutlet weak var backgroundImage: UIImageView!
-    @IBOutlet weak var swooshLogo: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    @IBAction func getStartedBtnWassPressed(_ sender: Any){
+        performSegue(withIdentifier: "leagueVCSegue", sender: self)
     }
     
     @IBAction func goBack(back: UIStoryboardSegue) {
